63ba829670524394f677d5bd7a2993e26373876f
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-04-30 14:05:56 -0400

    Cambios en la primara historia agregando condicion del select

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import data from './data/potter/potter.js'
 
 const dataPotter=data;
-const dataArray=Object.values(dataPotter);
+let dataArray=Object.values(dataPotter);
 
 const root=document.getElementById("root");
 for(let i=0;i<dataArray.length;i++){    
-   
-   /*root.innerHTML += `<img src="${dataArray[i].image}" alt="">`
-   root.innerHTML += `<p>Nombre: ${dataArray[i].name} <br> ${dataArray[i].house}</p>`*/
    document.getElementById('characters').innerHTML += `
    <div class="root">
         <img src="${dataArray[i].image}" alt="">
@@ -15,6 +12,37 @@ for(let i=0;i<dataArray.length;i++){
         <p>Especie: ${dataArray[i].house}</p>
     </div>`;
 }
+
+
+for(let i=0;i<dataArray.length;i++){    
+    document.getElementById('characters').innerHTML += `
+    <div class="root">
+        <img src="${dataArray[i].image}" alt="">
+        <h3>${dataArray[i].name}</h3>
+        <p>Especie: ${dataArray[i].house}</p>
+    </div>`;
+}
+
+const select = document.getElementById("alphabeticalSearch");
+
+select.addEventListener("change", () =>{
+    let condition = select.options[select.selectedIndex].index;
+    console.log(condition);
+
+    if(condition==1){
+        //Función que ordena alfabéticamente
+        dataArray.sort((a, b) => a.name.localeCompare(b.name));
+        console.log(dataArray);
+    }
+    if(condition==2){
+        //Arreglo con orden alfabético descendente
+        const dataArrayReverse=dataArray.reverse();
+        console.log(dataArrayReverse);
+    }
+
+})
+
+
 
 document.getElementById("homePage").style.display = "none";
 
