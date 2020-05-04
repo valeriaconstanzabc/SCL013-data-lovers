abb6b713e8bdfed904fd3d964b4a2c83f694639c
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-05-04 12:31:43 -0400

    Separacion de funciones en ambos archivos JS y orden de personajes de A-Z y Z-A

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
 export const anotherExample = () => {
   return 'OMG';
 };
+export const orderData = (dataArray, condition) => {
+  let filterOrder = dataArray; //Arreglo nuevo donde guardaremos persojes ordenados
+  if(condition == 1){
+    filterOrder.sort((a, b) => a.name.localeCompare(b.name));
+    return filterOrder;
+  }
+  if(condition==2){
+    const filterOrderReverse=filterOrder.reverse();
+    return filterOrderReverse;
+  }
+}
+export default orderData;
--- src/main.js
+++ src/main.js
@@ -1,57 +1,34 @@
 import data from './data/potter/potter.js'
-
+import orderData from './data.js';
 const dataPotter=data;
 let dataArray=Object.values(dataPotter);
-
-const root=document.getElementById("root");
-
+const characters = document.getElementById("characters");
 for(let i=0;i<dataArray.length;i++){    
-   let characters = document.getElementById('characters').innerHTML += `
+   characters.innerHTML += `
    <div class="root">
         <img src="${dataArray[i].image}" alt="">
         <h3>${dataArray[i].name}</h3>
         <p>${dataArray[i].house}</p>
     </div>`;
 }
-
 function vaciar() {
     characters.innerHTML = "";
 }
-
 const select = document.getElementById("alphabeticalSearch");
 select.addEventListener("change", () =>{
     vaciar();
     let condition = select.options[select.selectedIndex].index;
-    if(condition==1){
-        //Función que ordena alfabéticamente
-        dataArray.sort((a, b) => a.name.localeCompare(b.name));
-        
-        for(let i=0;i<dataArray.length;i++){    
-            document.getElementById('characters').innerHTML += `
-            <div class="root">
-                <img src="${dataArray[i].image}" alt="">
-                <h3>${dataArray[i].name}</h3>
-                <p>${dataArray[i].house}</p>
-            </div>`;
-        }
-    }if(condition==2){
-        //Arreglo con orden alfabético descendente
-        const dataArrayReverse=dataArray.reverse();
-        
-        for(let i=0;i<dataArrayReverse.length;i++){    
-            document.getElementById('characters').innerHTML += `
-            <div class="root">
-                <img src="${dataArrayReverse[i].image}" alt="">
-                <h3>${dataArrayReverse[i].name}</h3>
-                <p>${dataArrayReverse[i].house}</p>
-            </div>`;
-        }
+    let orderResult = orderData(dataArray, condition);
+    for(let i=0;i<orderResult.length;i++){    
+        let characters = document.getElementById('characters').innerHTML += `
+        <div class="root">
+             <img src="${orderResult[i].image}" alt="">
+             <h3>${orderResult[i].name}</h3>
+             <p>${orderResult[i].house}</p>
+         </div>`;
     }
 })
-
-
 document.getElementById("homePage").style.display = "none";
-
 let enterButton = document.getElementById("enterButton");
 enterButton.addEventListener('click', ()=>{
     document.getElementById("welcome").style.display = "none";
